f47e7977ef5c967154fa60c9b7c06b678c5ef155
Patrick Simonian <patricksimonian@gmail.com>
2016-11-14 12:51:52 -0800

    added example to test user email uniqueness and case sensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,13 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence:true, uniqueness: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 1}
 
 end
 
 
 # need email validator
+
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,10 +31,27 @@ RSpec.describe User, type: :model do
         password_confirmation: 'apples',
         email: 'bob@mail.com'
         })
-
       expect(@user2.save).to be false
     end
 
+    it 'should fail if email is not unqiue and case insensitive' do
+      @user1 = User.new({
+        first_name: 'bob',
+        last_name: 'rocks',
+        password: 'apples',
+        password_confirmation: 'apples',
+        email: 'bob@mail.com'
+        })
+      @user1.save
+      @user2 = User.new({
+        first_name: 'tob',
+        last_name: 'tocks',
+        password: 'apples',
+        password_confirmation: 'apples',
+        email: 'BOb@mail.Com'
+        })
+      expect(@user2.save).to be false
+    end
 
   end
 end
