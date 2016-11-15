79365a0d1b01226854b10a3060b1c13b9780746e
Patrick Simonian <patricksimonian@gmail.com>
2016-11-14 18:46:45 -0800

    bug: t/s unable to delete a product as admin if it has been used in a purchase

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,7 @@
 class LineItem < ActiveRecord::Base
 
   belongs_to :order
-  belongs_to :product
+  belongs_to :product, :dependent => :delete
 
   monetize :item_price_cents, numericality: true
   monetize :total_price_cents, numericality: true
