46cb2ea024813d864262f38bc0d6bb2784f100a2
Patrick Simonian <patricksimonian@gmail.com>
2016-11-15 17:08:37 -0800

    added some extra styling to review form, removed a type in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         session[:user_id] = @user.id
-          Tell the UserMailer to send a welcome email after save
+          # Tell the UserMailer to send a welcome email after save
         UserMailer.welcome_email(@user).deliver_now
         # format.json { render json: @user, status: :created, location: @user}
         format.html { redirect_to root_path }
